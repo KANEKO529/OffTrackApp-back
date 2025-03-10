1cd75e6be471963d741e099bcfa89a0dbe4f78bd
金子弘昌 <a5822036@aoyama.jp>
2025-03-10 20:28:10 +0900

    fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,9 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
+
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
 
